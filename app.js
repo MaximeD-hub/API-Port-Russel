a9c013b7c56831716c29fd1b2fef556eebc08dab
MaximeD <one.maximeduboispro@gmail.com>
2026-01-14 11:29:23 +0100

    start authentification

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
+const Catway = require("./models/Catway");
+const Reservation = require("./models/Reservation");
+
+
+//user Test
+const User = require("./models/User");
+
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
@@ -11,5 +19,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API Port de Russell opérationnelle 🚤");
 });
+
 
 module.exports = app;
